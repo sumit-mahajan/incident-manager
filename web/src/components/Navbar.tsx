007f6b364b882b57ed7d8d54f2689561b5689306
Sumit Mahajan <sumitmahajan2001@gmail.com>
2026-07-02 20:21:37 +0530

    fix: remove unused NavLink import in Navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ChevronDown, AlertCircle, User } from 'lucide-react';
 import { ThemeToggle } from './ThemeToggle';
 import { useUserStore } from '../features/auth/UserStoreProvider';
